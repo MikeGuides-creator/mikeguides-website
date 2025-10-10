89f10990321bf162c0105c8d5ce0b72f8f0825dd
Mike K. <mikeguidesblog@gmail.com>
2025-10-10 13:14:44 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./{blog,bundle,free,guides,products,store,toolkit}/**/*.html",
+    "./**/*.md",
+    "./**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: { 'mg-blue': '#2563eb', 'mg-green': '#22c55e' },
+      fontFamily: {
+        inter: ['Inter', 'ui-sans-serif', 'system-ui'],
+        poppins: ['Poppins', 'Inter', 'ui-sans-serif', 'system-ui'],
+      }
+    },
+  },
+  plugins: [],
+};
