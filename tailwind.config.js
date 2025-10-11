29c228e3653762fd2260c1c46043f7af0c141b2d
Mike K. <mikeguidesblog@gmail.com>
2025-10-11 09:44:10 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,9 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio")
+  ]
 };
