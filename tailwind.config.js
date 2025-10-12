3531e058050744b899a47381cc576f1614aa6f02
Mike K. <mikeguidesblog@gmail.com>
2025-10-12 18:38:57 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html",
-    "./{blog,bundle,free,guides,products,store,toolkit}/**/*.html",
-    "./**/*.md",
-    "./**/*.js",
-    "!./node_modules/**"
+    './*.html',
+    './{products,guides,blog,bundle,free,store,toolkit}/**/*.html',
+    './**/*.md',
+    './**/*.js'
   ],
   theme: {
     extend: {
-      colors: { "mg-blue": "#2563eb", "mg-green": "#22c55e" },
-      fontFamily: {
-        inter: ["Inter", "ui-sans-serif", "system-ui"],
-        poppins: ["Poppins", "Inter", "ui-sans-serif", "system-ui"]
-      }
+      fontFamily: { poppins: ['Poppins','ui-sans-serif','system-ui'] },
+      colors: { 'mg-blue': '#2563eb' }
     }
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio")
-  ]
-};
+  plugins: []
+}
