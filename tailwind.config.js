5ce6e1875f22ff0fda1887acf2d923849bf6f5d7
Mike K. <mikeguidesblog@gmail.com>
2025-10-12 18:54:59 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './*.html',
-    './{products,guides,blog,bundle,free,store,toolkit}/**/*.html',
-    './**/*.md',
-    './**/*.js'
-  ],
-  theme: {
-    extend: {
-      fontFamily: { poppins: ['Poppins','ui-sans-serif','system-ui'] },
-      colors: { 'mg-blue': '#2563eb' }
-    }
-  },
-  plugins: []
-}
+content: [
+  './*.html',
+  './{products,guides,blog,bundle,free,store,toolkit}/**/*.html',
+  './assets/js/**/*.js',
+  './**/*.md'
+],
