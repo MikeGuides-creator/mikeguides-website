e4e66b465b1e11f61104c3fba7b26f3922098e0c
Mike K. <mikeguidesblog@gmail.com>
2025-12-28 18:07:08 -0600

    Create ga-init.js

---
+++ assets/js/ga-init.js
@@ -0,0 +1,6 @@
+// GA4 init (CSP-safe: no inline script needed)
+window.dataLayer = window.dataLayer || [];
+window.gtag = function () { window.dataLayer.push(arguments); };
+
+window.gtag('js', new Date());
+window.gtag('config', 'G-PHWLTS997T');
